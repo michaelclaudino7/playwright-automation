4decd7bb3a32a748cd5d63c791e1f24e0b1d9fb6
michaelclaudino7 <michaelclaudino7@gmail.com>
2026-08-14 23:55:37 -0300

    feat: initial project setup with pytest and Playwright config

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://www.saucedemo.com"
+DEFAULT_TIMEOUT = 10000  # milissegundos
+
+STANDARD_USER = "standard_user"
+LOCKED_OUT_USER = "locked_out_user"
+PROBLEM_USER = "problem_user"
+PASSWORD = "secret_sauce"
